ff1e24eab657f9c77fa9120a09a8ded455dd2e73
Micha <micha.pdm@gmx.de>
2021-12-11 22:04:06 +0100

    content: replaced e-mail addresses

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,23 +1,23 @@
 import react from "react";
 
 const Contacts = () => {
-    return (
-   
-            <div className="names">
-            <h1 className="info"> This project is made with love by : <br/> </h1>
-           
-          <h5 className="name">  Dr. Michael Breitenstein</h5>
-          <a className="link1" href="mailto:micha.pdm@gmx.de">Click here for my email </a>
-     
-           <h5 className="name"> Mrs.Boryana Dimitrova </h5> 
-           <a href="mailto:boryana.dimitrova789@gmail.com">Click here for my email </a>
-            
-            
-         
-            </div>
-                
-      
-    )
+  return (
+
+    <div className="names">
+      <h1 className="info"> This project is made with love by : <br /> </h1>
+
+      <h5 className="name">  Dr. Michael Breitenstein</h5>
+      <a className="link1" href="mailto:placeholder">Click here for my email </a>
+
+      <h5 className="name"> Mrs.Boryana Dimitrova </h5>
+      <a href="mailto:placeholder">Click here for my email </a>
+
+
+
+    </div>
+
+
+  )
 };
 
 export default Contacts;
